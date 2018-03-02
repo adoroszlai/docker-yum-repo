76df93b6b472ac61dd582cfe873eac5341ad5531
Doroszlai, Attila <adoroszlai@apache.org>
2018-03-02 23:33:12 +0100

    option to copy only some packages

--- README.md
+++ README.md
@@ -7,16 +7,23 @@ Repos will be mirrored to `REPO_DIR` on the host.
 ```bash
 REPO_DIR=~/data/repos
 
+# Copy full YUM repo
 # docker build -t copy-yum-repo copy-yum-repo
 docker run --rm -v $REPO_DIR:/var/repo copy-yum-repo \
   http://public-repo-1.hortonworks.com/HDP/centos6/2.x/updates/2.5.0.0/hdp.repo \
   http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.4.0.1/ambari.repo
 
+# Download only some packages
+docker run --rm -v $REPO_DIR:/var/repo copy-yum-repo \
+  pkg http://public-repo-1.hortonworks.com/HDP/centos6/2.x/updates/2.5.0.0/hdp.repo "kafka*" "zookeeper*"
+
+# Copy APT mirror
 # docker build -t apt-mirror apt-mirror
 docker run --rm -v $REPO_DIR:/var/spool/apt-mirror/mirror apt-mirror \
   http://public-repo-1.hortonworks.com/HDP/debian7/2.x/updates/2.6.3.0/hdp.list \
   http://public-repo-1.hortonworks.com/ambari/debian7/2.x/updates/2.6.0.0/ambari.list
 
+# Run nginx to serve local repos
 # docker build -t serve-yum-repo serve-yum-repo
 docker run -d --name yum-repo -p 80:80 -h yum-repo -v $REPO_DIR:/var/repo serve-yum-repo
 ```
--- copy-yum-repo/entrypoint.sh
+++ copy-yum-repo/entrypoint.sh
@@ -1,16 +1,78 @@
 #!/bin/bash
 # vim: ts=2 sw=2 sts=2 et
 
-set -e
-set -u
+set -e -u
 
-for repo_url in "$@"; do
-  repo_file="/etc/yum.repos.d/$(basename $repo_url)"
-  curl -s -o "$repo_file" "$repo_url"
-  for line in $(perl -wnl -00 -e '($name, $path) = $_ =~ /\[(\S+)\].*baseurl=https?:\/\/[^\/]+\/(\S+)/s and print "$name]$path"' "$repo_file"); do
-    repo="$(echo $line | cut -f1 -d']')"
-    path="$(echo $line | cut -f2 -d']')"
-    reposync -n -p /var/repo/$path --norepopath -r "$repo" && createrepo --update /var/repo/$path
+function get-repo-file {
+  local repo_url="$1"
+  cd /etc/yum.repos.d
+  rm -f *.repo
+  curl -s -O "${repo_url}"
+  echo $(pwd -P)/$(basename ${repo_url})
+}
+
+function copy-packages {
+  local repo_url=$1
+  shift
+
+  local repo_file=$(get-repo-file "${repo_url}")
+
+  local urls=$(repotrack -n --urls "$@")
+  for url in ${urls}; do
+    local dir=$(echo ${url} | cut -f3 -d'/')
+    local path=$(echo ${url} | cut -f4- -d'/')
+    mkdir -p ${dir}
+    cd ${dir}
+    echo "Downloading ${url}"
+    curl --create-dirs -o ${path} -s ${url}
+    cd ..
   done
-  rm -f "$repo_file"
-done
+
+  for dir in $(grep 'baseurl=' ${repo_file} | cut -f3- -d'/'); do
+    [[ -d ${dir} ]] || continue
+    echo "Creating repo in ${dir}"
+    createrepo -q --update /var/repo/${dir}
+  done
+
+  rm -f "${repo_file}"
+}
+
+function copy-repo {
+  for repo_url in "$@"; do
+    local repo_file=$(get-repo-file "${repo_url}")
+    for line in $(perl -wnl -00 -e '($name, $path) = $_ =~ /\[(\S+)\].*baseurl=https?:\/\/(\S+)/s and print "$name]$path"' "${repo_file}"); do
+      local repo="$(echo ${line} | cut -f1 -d']')"
+      local path="$(echo ${line} | cut -f2 -d']')"
+      reposync -n -p /var/repo/${path} --norepopath -r "${repo}"
+      echo "Creating repo in ${path}"
+      createrepo -q --update /var/repo/${path}
+    done
+    rm -f "${repo_file}"
+  done
+}
+
+function usage {
+  echo "Usage:"
+  echo "  repo <repo_url> [<repo_url> ...]"
+  echo "  pkg <repo_url> <pkg_name> [<pkg_name> ...]"
+}
+
+cmd=${1:-help}
+
+case ${cmd} in
+  pkg)
+    shift
+    copy-packages "$@"
+    ;;
+  repo)
+    shift
+    copy-repo "$@"
+    ;;
+  http*)
+    # backwards compatibility
+    copy-repo "$@"
+    ;;
+  *)
+    usage
+    ;;
+esac
